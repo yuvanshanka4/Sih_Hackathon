970c6d6e1c5ee4f7d8766a3d3cf78280971f0975
yuvanshanka4 <128825673+yuvanshanka4@users.noreply.github.com>
2023-09-23 07:23:49 +0530

    Delete top_facts directory

--- top_facts/new.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu');
-    const navMenu = document.getElementById('menubar');
-
-    menuToggle.addEventListener('click', function () {
-        navMenu.classList.toggle('menubar');
-    });
-});
